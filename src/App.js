596ae466c6f2a06b013c8fd501e6afcfe8ceb32d
VladimirBabchenko <mgrinevitch@gmail.com>
2020-03-15 12:26:49 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          id: "asc1",
+          name: "Frankenstein"
+        },
+        {
+          id: "asr2",
+          name: "Dracula"
+        },
+        {
+          id: "as1w",
+          name: "Zombie"
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}> {monster.name} </h1>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
