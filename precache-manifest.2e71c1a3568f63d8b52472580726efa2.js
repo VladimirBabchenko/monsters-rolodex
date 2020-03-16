561c8dac19f44dfd1b70397366c254af51a3e387
VladimirBabchenko <mgrinevitch@gmail.com>
2020-03-16 22:47:20 +0200

    Updates

---
+++ precache-manifest.2e71c1a3568f63d8b52472580726efa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6821645fcaff29da4ae08e30381f5ff",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "04615d59a6849750592e",
+    "url": "/monster-rolodex/static/css/main.3a10e3f0.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monster-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04615d59a6849750592e",
+    "url": "/monster-rolodex/static/js/main.3d8038da.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
